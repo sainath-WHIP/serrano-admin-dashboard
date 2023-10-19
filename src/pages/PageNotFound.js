3c8b94c7bd72a55ac08b42baae8ea4be6a9dc9a9
sainath-WHIP <akhilp@whiprides.com>
2023-10-19 13:29:58 +0530

    third commit

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,11 +1,10 @@
 function PageNotFound() {
   return (
     <>
-      <div className="text-2xl text-black flex justify-center items-center bg-white">
-        404 PageNotFound
+      <div className="flex flex-col justify-center items-center h-screen bg-[#c4c4c4]">
+        <div className="text-2xl text-black font-medium">404 PageNotFound</div>
+        <a href="/dashboard" className="mt-6 text-black text-base font-medium hover:underline">Back to Dashboard</a>
       </div>
-
-      <a href="/dashboard">Back to Dashboard</a>
     </>
   );
 }
